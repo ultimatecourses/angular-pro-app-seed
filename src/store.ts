1d10984bbe2b7899c48da29a1d478ab8997c8548
Todd Motto <todd@toddmotto.com>
2017-07-22 16:05:06 +0100

    Update Store

--- src/store.ts
+++ src/store.ts
@@ -6,16 +6,19 @@ import 'rxjs/add/operator/distinctUntilChanged';
 
 import { User } from './auth/shared/services/auth/auth.service';
 import { Meal } from './health/shared/services/meals/meals.service';
+import { Workout } from './health/shared/services/workouts/workouts.service';
 
 export interface State {
   user: User,
   meals: Meal[],
+  workouts: Workout[],
   [key: string]: any
 }
 
 const state: State = {
   user: undefined,
   meals: undefined,
+  workouts: undefined,
 };
 
 export class Store {
