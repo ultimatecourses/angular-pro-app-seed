f6e3825f691d32217731387a0ae84e4e28ffbd52
dev-lester <lesters821@gmail.com>
2022-01-08 04:35:01 +0800

    initial commit firebase auth module login register shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { Store } from 'store';
 
 // feature modules
+import { AuthModule } from '../auth/auth.module';
 
 // containers
 import { AppComponent } from './containers/app/app.component';
@@ -17,7 +18,8 @@ export const ROUTES: Routes = [];
 @NgModule({
   imports: [
     BrowserModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
+    AuthModule
   ],
   declarations: [
     AppComponent
@@ -30,3 +32,16 @@ export const ROUTES: Routes = [];
   ]
 })
 export class AppModule {}
+
+/*
+const firebaseConfig = {
+  apiKey: "AIzaSyDqxrBJszBCola9BVhMHzqSSYw7JqtUCUw",
+  authDomain: "angular-fitness-app-4965b.firebaseapp.com",
+  databaseURL: "https://angular-fitness-app-4965b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "angular-fitness-app-4965b",
+  storageBucket: "angular-fitness-app-4965b.appspot.com",
+  messagingSenderId: "190811107468",
+  appId: "1:190811107468:web:63d1286e2e3a1a74888ba9",
+  measurementId: "G-HT9P47SY8R"
+};
+*/
